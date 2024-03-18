426c149e5c8e525a7da23db349f1a73a2508083e
DazFather <The6daz6@Gmail.com>
2024-03-18 20:27:14 +0100

    "Add checks for temrinal command"

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"os"
 	"strings"
 )
@@ -16,11 +17,18 @@ func main() {
 		term.ShowWarning(err)
 	}
 
-	if IsFlag(os.Args[1], "terminal") {
-		err = term.InteractiveGitp("", len(os.Args) > 2 && IsFlag(os.Args[2], "keep-alive"))
-	} else {
+	if !IsFlag(os.Args[1], "terminal") {
 		term.Cursor(strings.Join(os.Args[1:], " ") + "\n")
 		err = term.Gitp(os.Args[1], os.Args[2:]...)
+	} else if nArgs := len(os.Args); nArgs == 2 {
+		err = term.InteractiveGitp("", false)
+	} else if name, _, valid := ParseFlag(os.Args[2]); !valid || name != "keep-alive" {
+		err = errors.New("Invalid argument: the only flag allowed for 'gitp terminal' is: --keep-alive")
+	} else {
+		if nArgs > 3 {
+			term.ShowWarning("Too many arguments, ignoring unnecessary ones")
+		}
+		term.InteractiveGitp("", true)
 	}
 
 	if err != nil {
